d327f2ed52ed245e53fd13873c65901334d2cdad
PiotrW-bit <piotrwoj@student.agh.edu.pl>
2022-01-07 12:14:20 +0100

    Organizacja

--- recommender.py
+++ recommender.py
@@ -4,13 +4,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def tag_transformer(df1, path):
-    for i, row in df1.iterrows():
-        df1.loc[i, 'tags'] = f'{row.tag_1}, {row.tag_2}'
-
-    df1.pop('tag_1')
-    df1.pop('tag_2')
-    df1.to_csv(path)
+def tag_transformer(df, path):
+    for i, row in df.iterrows():
+        df.loc[i, 'tags'] = f'{row.tag_1}, {row.tag_2}'
+    df.pop('Unnamed: 0')
+    df.pop('tag_1')
+    df.pop('tag_2')
+    df.to_csv(path)
 
 
 def create_tags(x):
@@ -42,7 +42,7 @@ def get_recommendations(cosine_sim, idx=-1):
     # Return the top 10 most similar dishes
     return food_indices
 
-df1 = pd.read_csv('./dania_glowne_database.csv')
 
+df1 = pd.read_csv('db/dania_glowne_database.csv')
 df1['tager'] = df1.apply(create_tags, axis=1)
 indices_from_food_id = pd.Series(df1.index, index=df1['food_id'])
