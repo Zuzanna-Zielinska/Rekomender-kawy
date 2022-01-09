4904752019752ec4b653b9cfdf4d65102ce26d78
Zuzanna Zielińska <zzielinska@student.agh.edu.pl>
2022-01-09 21:33:56 +0100

    Dopracowanie szkieletu GUI
    Poprawa struktury GUI
    Dodanie szkieletu strony kreacji użytkownika i rekomendacji
    Dodanie przełączania się między stronami

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@
 # wybór w nim będzie po prostu przyciskami create_user i login, po kliknięciu login 
 # wybierać się będzie usera jeszcze jeden przycisk można dodać delete_user i po kliknięciu 
 # się będzie wybierać usera to też bym jako pop_up zrobił?
+#Zuzia: tutaj też bym dała pole wyboru użytkownika, tzn jest na to miejsce
 
 # TODO: create_user window
 # zrobić pole na podanie imienia i nazwiska
@@ -21,48 +22,92 @@ from tkinter import *
 
 """
 Interfejs wyświetlania okna.
-Próbowałam stowować zasady SOLID.
+Tak, próbowałam stowować zasady SOLID XD.
 
 Przyjmuje:
     width - szerokość okna
     height - wysokość okna
-
 """
-class IWindow():
+class I_Window():
     def __init__(self, width, height):
-        self.style_table()
-        
         self.window = Tk()  # tworzenie okna
         self.window.geometry('{0}x{1}'.format(width, height))
+        
+    
+    def mainloop(self):
+        self.window.mainloop()
+        
+"""
+Interfejs wyświetlania strony.
+
+Przyjmuje:
+    window - okno programu
+"""
+class I_Page():
+    def __init__(self, window):
+        self.window = window
+        
+        self.style_table()
         
     def style_table(self):
         """
         Lista czcionek, ich wysokości, wymiarów przycisków.
         """
         self.title_font = ["lucida", 40]
+        self.minor_title_font = ["lucida", 30] 
         self.button_font = ["lucida", 20]
         self.normal_text_font = ["lucida", 12]
         
         self.button_size = [20, 0]
-    
-    def mainloop(self):
-        self.window.mainloop()
-
         
-"""
-Okno wyboru użytkownika
-"""
-class Start_window(IWindow):
+    def clear_window(self):
+        """
+        Funkcja czysci okno z wszelkich elementow.
+        """   
+        
+        for child in self.window.winfo_children():
+            child.destroy()
+            
+    #TODO: uniwersalna funkcja przełączania strony (poniższa jeszcze nie działa)
+    def change_page(self, New_Page):
+        '''
+        Przełączenie do innej strony
+        '''
+        
+        self.clear_window()
+        Create_User_Page(self.window)
     
+
+"""
+Główne okno programu. Inicjuje stronę startową.
+
+Przyjmuje:
+    width - szerokość okna
+    height - wysokość okna
+"""
+class Main_Window(I_Window):
     def __init__(self, *args, **kwargs):
         
+        super().__init__(*args, **kwargs) #załadowanie initu z rodzica
+        Start_Page(self.window)
+    
+        
+"""
+Strona wyboru użytkownika
+
+Przyjmuje:
+    window - okno programu
+"""
+class Start_Page(I_Page):
+    
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) #załadowanie initu z rodzica
         self.layout()
         
     
     def layout(self):
         '''
-        Ułożenie i wyswietlenie przycisków na stronie
+        Ułożenie i wyswietlenie wszystkiego na stronie
         '''
         
         # ----elementy w oknie dopasowują się do jego rozmiaru----
@@ -79,18 +124,137 @@ class Start_window(IWindow):
                       text="Rekomender", font=self.title_font)
         title.pack()
         # ------------------------------------
-        # --------przycisk wczytywania--------
+        # ------------przyciski------------
         self.buttons_frame = Frame(master = self.window)
         self.buttons_frame.grid(row=1, column=0, sticky="n")
         
         self.button_login = Button( master = self.buttons_frame, width = self.button_size[0],
-            height = self.button_size[1], text="Wybierz użytkownika", font = self.button_font)
+            height = self.button_size[1], text="Wybierz użytkownika", font = self.button_font, 
+            command=(lambda: self.change_to_recommendation_page()))
         self.button_login.pack()
         
         self.button_create_user = Button( master = self.buttons_frame, width = self.button_size[0],
-            height = self.button_size[1], text="Dodaj użytkownika", font = self.button_font)
+            height = self.button_size[1], text="Dodaj użytkownika", font = self.button_font, 
+            command=(lambda: self.change_to_create_user_page()))
         self.button_create_user.pack()
+        # ----------------------------------
+
+    def change_to_create_user_page(self):
+        '''
+        Zmień na stronę tworzenia urzytkownika
+        '''
+        
+        self.clear_window()
+        Create_User_Page(self.window)
+        
+    def change_to_recommendation_page(self):
+        '''
+        Zmień na stronę z rekomendacjami
+        '''
+        
+        self.clear_window()
+        Recommendation_Page(self.window)
+
+"""
+Strona tworzenia urzytkownika
+
+Przyjmuje:
+    window - okno programu
+"""        
+class Create_User_Page(I_Page):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) #załadowanie initu z rodzica
+        self.layout()
+        
+    def layout(self):
+        '''
+        Ułożenie i wyswietlenie wszystkiego na stronie
+        '''
+        
+        # ----elementy w oknie dopasowują się do jego rozmiaru----
+        self.window.columnconfigure(0, weight=1)
+        for i in range(2):
+            self.window.rowconfigure(i, weight=1)
+        # --------------------------------------------------------
+        
+        # ---------------tytuł----------------
+        self.title_frame = Frame(master = self.window)
+        self.title_frame.grid(row=0, column=0, sticky="n")
+
+        title = Label(master=self.title_frame,
+                      text="Kreacja użytkownika", font=self.minor_title_font)
+        title.pack()
         # ------------------------------------
         
-open_window = Start_window(500, 580)
+        # ------------przyciski------------
+        self.buttons_frame = Frame(master = self.window)
+        self.buttons_frame.grid(row=1, column=0, sticky="n")
+        
+        self.button_go_back = Button( master = self.buttons_frame, width = self.button_size[0],
+            height = self.button_size[1], text="Zatwierdź wybór", font = self.button_font, 
+            command=(lambda: self.change_to_start_page()))
+        self.button_go_back.pack()
+        # ----------------------------------
+    
+    def change_to_start_page(self):
+        '''
+        Powrót do strony startowej
+        '''
+        
+        self.clear_window()
+        Start_Page(self.window)
+
+"""
+Strona z rekomendacjami
+
+Przyjmuje:
+    window - okno programu
+"""      
+class Recommendation_Page(I_Page):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) #załadowanie initu z rodzica
+        self.layout()
+        
+    def layout(self):
+        '''
+        Ułożenie i wyswietlenie wszystkiego na stronie
+        '''
+        
+        # ----elementy w oknie dopasowują się do jego rozmiaru----
+        self.window.columnconfigure(0, weight=1)
+        for i in range(2):
+            self.window.rowconfigure(i, weight=1)
+        # --------------------------------------------------------
+        
+        # ---------------tytuł----------------
+        self.title_frame = Frame(master = self.window)
+        self.title_frame.grid(row=0, column=0, sticky="n")
+
+        title = Label(master=self.title_frame,
+                      text="Dieta dla ciebie!", font=self.minor_title_font)
+        title.pack()
+        # ------------------------------------
+        
+        # ------------przyciski------------
+        self.buttons_frame = Frame(master = self.window)
+        self.buttons_frame.grid(row=1, column=0, sticky="n")
+        
+        self.button_go_back = Button( master = self.buttons_frame, width = self.button_size[0],
+            height = self.button_size[1], text="Powrót do strony startowej", font = self.button_font, 
+            command=(lambda: self.change_to_start_page()))
+        self.button_go_back.pack()
+        # ----------------------------------
+        
+    def change_to_start_page(self):
+        '''
+        Powrót do strony startowej
+        '''
+        
+        self.clear_window()
+        Start_Page(self.window)
+        
+        
+open_window = Main_Window(500, 580)
 open_window.mainloop()
