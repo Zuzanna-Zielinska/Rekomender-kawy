c1aa166a02346dcd1e61738fcf5f864a8f792dbc
PiotrW-bit <piotrwoj@student.agh.edu.pl>
2022-01-12 21:34:30 +0100

    Funkcja dodająca użytkowników

--- user.py
+++ user.py
@@ -1,5 +1,30 @@
 import pandas as pd
-# user_id od  0 do 1
+import numpy as np
+
 # liked_diets i disliked_diets id diety z tabeli diet, które będziemy w df zapisywać już w recommender window do implementacji
 # preferences dane podane przy zakładaniu konta w gui ten df będzie tworzony w create_user window
-users = pd.DataFrame(columns=['user_id', 'imie',  'nazwisko', 'liked_diets', 'dislike_diets', 'preferences'])
+
+'''
+Funkcja tworząca użytkownika przyjmująca 3 stringi. Jest możliwość przyjmowania listy preferencji
+'''
+def create_user(imie, nazwisko, preferencje):
+    df = pd.read_csv('db/users.csv')
+    idx = df['user_id'].values
+    for count, idx in enumerate(set(sorted(idx))):
+        if count+1 != idx:
+            new_idx = count + 1
+            break
+        else:
+            new_idx = count + 2
+    if df['user_id'].values.size == 0:
+        new_idx = 1
+    new_user = {'user_id': new_idx, 'imie': imie, 'nazwisko': nazwisko, 'liked_diets': np.NAN, 'dislike_diets': np.NAN, 'preferences': preferencje}
+    df = df.append(new_user, ignore_index=True)
+    df.to_csv('db/users.csv', index=False)
+
+
+create_user('adam', 'wojniak', 'angi cos ans')
+
+
+def delete_user():
+    pass
